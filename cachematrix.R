42e89832ae40ef59c71974b7ab03f1856ebd458b
anguillanneuf <tianzih@outlook.com>
2014-11-22 11:16:27 -0500

    Update cachematrix.R
    These are my codes for Programming Assignment 2. I have tested them on my R console, and they seem to work.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that cache the inverse of a matrix when it is
+## absent and create the inverse of a matrix when it is present
 
-## Write a short comment describing this function
+
+## Create a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+	i <<- NULL
 
+	set<- function(y){
+		x <<- y
+		i <<- NULL
+	}
+
+	get <- function() x
+	setInverse <- function(inverse) i <<- inverse
+	getInverse <- function() i
+	list(get = get, set=set, getInverse = getInverse, 
+	setInverse = setInverse)
 }
 
-
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	i <- x$getInverse()
+
+      if (!is.null(i)) {
+		message("getting cached inverse")
+            return(i)
+      }
+
+      y <- x$get()
+      i <- solve(y)
+      x$setInverse(i)
+      i
 }
